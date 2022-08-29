46df90ed0dc11d4c4b923082208cc641b6b6cf58
isheryt <92232128+isheryt@users.noreply.github.com>
2022-08-29 19:48:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var name1=localStorage.getItem("uname");
+document.getElementById("welcome").innerHTML="welcome "+name1;
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
---
+++ wreck.js
@@ -0,0 +1,5 @@
+function lg(){
+    var un=document.getElementById("kw").value;
+    localStorage.setItem("uname",un);
+    window.location="wreck_room.html"
+    }
